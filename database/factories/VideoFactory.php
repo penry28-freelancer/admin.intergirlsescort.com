471a6d6980cea874929143e07084bfeb4542cd74
luccui000 <luccui2k@gmail.com>
2022-10-06 12:46:29 +0700

    [ADDED] Add video factory

---
+++ database/factories/VideoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VideoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'escort_id' => random_int(1, 100),
+            'path' => $this->faker->url(),
+            'views' => random_int(0, 10000),
+            'type' => 'mp4',
+            'duration' => random_int(10, 100),
+        ];
+    }
+}
