85e04509dd68e06d7b4247c735970a7c2eb955b1
penry28 <phamdinhhung28.it@gmail.com>
2022-10-09 15:41:31 +0700

    [DONE] Api add & get account favorite

--- app/Repositories/Account/AccountRepository.php
+++ app/Repositories/Account/AccountRepository.php
@@ -6,18 +6,8 @@ use App\Repositories\EloquentRepository;
 
 class AccountRepository extends EloquentRepository implements AccountRepositoryInterface
 {
-
     public function model()
     {
         return \App\Models\Account::class;
-    }
-
-    public function filterFavoritesByType($type)
-    {
-        // $modelName =
-        $builder = $this->model;
-
-        // $builder = $builder->where('accountable_type', $modelName);
-        return $builder->get();
     }
 }
