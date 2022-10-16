e6834ada9c7a74793d7305d30c87efeee66af41d
luccui000 <luccui2k@gmail.com>
2022-10-16 15:59:21 +0700

    [ADDED] Add escort review

--- database/factories/EscortReviewFactory.php
+++ database/factories/EscortReviewFactory.php
@@ -15,15 +15,20 @@ class EscortReviewFactory extends Factory
      */
     public function definition()
     {
+        $escort_ids = DB::table('escorts')->pluck('id')->toArray();
+        $country_ids = DB::table('countries')->pluck('id')->toArray();
+        $city_ids = DB::table('cities')->pluck('id')->toArray();
+        $currency_ids = DB::table('currencies')->pluck('id')->toArray();
+
         return [
             'nickname' => $this->faker->name(),
-            'country_id' => random_int(1, DB::table('countries')->count()),
-            'city_id' => random_int(1, DB::table('cities')->count()),
-            'escort_id' => 1,
+            'country_id' => $country_ids[array_rand($country_ids)],
+            'city_id' => $city_ids[array_rand($city_ids)],
+            'escort_id' => $escort_ids[array_rand($escort_ids)],
             'meeting_date' => Carbon::now(),
             'meeting_length' => 'Metting length',
-            'price' => random_int(1000, 10000),
-            'currency_id' => 1,
+            'price' => random_int(100, 100000),
+            'currency_id' => $currency_ids[array_rand($currency_ids)],
             'rating' => random_int(1, 5),
             'comment' => $this->faker->paragraph(),
             'is_verified' => random_int(0, 1),
