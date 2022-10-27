84fe8c5fefe08e2f3b48a04495df67a812b64a10
penry28 <phamdinhhung28.it@gmail.com>
2022-10-27 21:28:59 +0700

    [ADDED] factories faker languages of escort

--- database/factories/EscortFactory.php
+++ database/factories/EscortFactory.php
@@ -24,10 +24,11 @@ class EscortFactory extends Factory
             $escort->account_id = $account->id;
             $escort->save();
 
-            $languages_ids = \DB::table('languages')->tack(4)->pluck('id')->toArray();
-            dd($languages_ids);
+            $languages_ids = \DB::table('languages')->pluck('id')->toArray();
+            $languages_random_ids = array_rand($languages_ids, 4);
 
-            // $escort->languages()->sync();
+            dd($languages_random_ids);
+            $escort->languages()->sync($languages_random_ids);
 
             $escort->videoInfo()->create([
                 'name'       => $this->faker->name(),
